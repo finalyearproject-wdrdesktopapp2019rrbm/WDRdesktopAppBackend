c61c083ac17df28fd91d825ac0a6782afa091e21
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-20 20:19:28 +0300

    yes_testing

--- models/User.js
+++ models/User.js
@@ -2,23 +2,24 @@ var db = require('../dbconnection');
 
 var User = {
     getAllUsers: function(callback) {
-        return db.query("Select * from systemusers", callback);
+        return db.query("Select * from Users", callback);
     },
     getUserById: function(id, callback) {
-      return db.query("Select * from systemusers where Userid = ?", [id], callback);
+      return db.query("Select * from Users where id = ?", [id], callback);
     },
     addUser: function(User, callback) {
-        let sql = 'INSERT INTO systemusers (name, username, email, password) VALUES (?,?,?,?)';
-        return db.query(sql,[User.name, User.username, User.email, User.password], callback );
+        let sql = 'INSERT INTO Users (username, email, password) VALUES (?,?,?)';
+        return db.query(sql,[User.username, User.email, User.password], callback );
         
 
       },
       deleteUser: function(id, callback) {
-        return db.query("delete from systemusers where Id = ?", [id], callback);
+        return db.query("delete from Users where Id = ?", [id], callback);
       },
       updateUser: function(id, User, callback){
-          let sql = "UPDATE systemusers SET name=?, username=? , email=?, password=? WHERE Userid =?";
-          let data = [User.name, User.username, User.email, User.password, id];
+        console.log(User);
+          let sql = "UPDATE Users SET username=? , email=?, password=? WHERE id =?";
+          let data = [User.username, User.email, User.password, id];
           return db.query(sql, data, callback );
       }
 };
